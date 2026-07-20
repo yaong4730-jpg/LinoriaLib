0f1edf476003be2f164b6e6da318232037b113ec
yaong4730-jpg <yaong4730@gmail.com>
2026-07-20 19:16:03 +0900

    Update repository URL for LinoriaLib

--- Example.lua
+++ Example.lua
@@ -1,4 +1,4 @@
-local repo = "https://raw.githubusercontent.com/mstudio45/LinoriaLib/main/"
+local repo = "https://raw.githubusercontent.com/yaong4730-jpg/LinoriaLib/main/"
 
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
